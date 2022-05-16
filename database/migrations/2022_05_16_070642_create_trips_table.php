0c495b3b2ed423968bfd6d095ab9efd5e709e241
Calvin  Tangeman <84664@b5-450-55.ict.lab.locals>
2022-05-16 13:35:20 +0200

    Inital commit

---
+++ database/migrations/2022_05_16_070642_create_trips_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTripsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('trips', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->string('destination');
+            $table->longText('description');
+            $table->date('start_date');
+            $table->date('end_date');
+            $table->integer('max_registrations');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('trips');
+    }
+}

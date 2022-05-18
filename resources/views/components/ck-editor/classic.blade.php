7acc5bf96adb38e219574d5491f9eea9dca291ba
Calvin  Tangeman <84664@b5-450-55.ict.lab.locals>
2022-05-18 10:02:44 +0200

    nog een bug fix lol

--- resources/views/components/ck-editor/classic.blade.php
+++ resources/views/components/ck-editor/classic.blade.php
@@ -1,1 +1,1 @@
-<textarea id="editor" name="{{ $name }}" {{ $required ?? null }} style="display: none" placeholder="{{ $placeholder }}">{{ $slot }}</textarea>
+<textarea id="editor" name="{{ $name }}" {{ $required ?? null }} style="display: none" placeholder="{{ $placeholder ?? null }}">{{ $slot }}</textarea>
